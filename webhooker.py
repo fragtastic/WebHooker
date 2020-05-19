6512b70e39c5562b02f4e36c7e72286a679df781
fragtastic <fragtastic@users.noreply.github.com>
2020-05-18 21:43:00 -0700

    Cleaned up flow

--- webhooker.py
+++ webhooker.py
@@ -15,42 +15,41 @@ class WebHooker(BaseHTTPRequestHandler):
         with open('webhooker.json') as json_data_file:
             config = json.load(json_data_file)
         xhubsignature = self.headers.get('X-Hub-Signature')
-        if xhubsignature is not None:
-            sha_type, signature = xhubsignature.split('=')
-            if sha_type == 'sha1':
-                content_length = int(self.headers.get('Content-Length'))
-                if content_length > 0:
-                    payloadb = self.rfile.read(content_length)
-                    payloads = payloadb.decode('UTF-8')
-                    payloadj = json.loads(payloads)
-                    repository = payloadj['repository']['full_name']
-                    if repository in config:
-                        secret = config[repository]['secret']
-                        mac = hmac.new(bytes(secret.encode('ascii')), msg=payloadb, digestmod=hashlib.sha1)
-                        if str(mac.hexdigest()) == str(signature):            
-                            event = self.headers.get('X-Github-Event')
-                            if event == 'ping':
-                                self.send_complex_response(200, 'Success')
-                            elif event == 'push':
-                                    branch = payloadj['ref'].split('/')[2]
-                                    command = config[repository][event][branch]['command']
-                                    try:
-                                        body = check_output(command)
-                                        self.send_complex_response(200, 'Success', body)
-                                    except:
-                                        self.send_complex_response(500, 'Something went wrong')
-                            else:
-                                self.send_complex_response(500, 'Unsupported event')
-                        else:
-                            self.send_complex_response(403, 'Invalid signature')
-                    else:
-                        self.send_complex_response(500, 'Unsupported repository')
-                else:
-                    self.send_complex_response(403, 'Invalid content length')
-            else:
-                self.send_complex_response(403, 'Unsupported hash type')
+        print(self.headers)
+        if xhubsignature is None:
+            return self.send_complex_response(403, 'No "X-Hub-Signature" signature')
+        sha_type, signature = xhubsignature.split('=')
+        print(sha_type, signature)
+        if sha_type != 'sha1':
+            return self.send_complex_response(403, 'Unsupported hash type')
+        content_length = int(self.headers.get('Content-Length'))
+        if content_length <= 0:
+            return self.send_complex_response(403, 'Invalid content length')
+        payloadb = self.rfile.read(content_length)
+        payloads = payloadb.decode('UTF-8')
+        # print(payloads)
+        payloadj = json.loads(payloads)
+        repository = payloadj['repository']['full_name']
+        if repository not in config:
+            return self.send_complex_response(500, 'Unsupported repository')
+        secret = config[repository]['secret']
+        mac = hmac.new(bytes(secret.encode('ascii')), msg=payloadb, digestmod=hashlib.sha1)
+        if str(mac.hexdigest()) != str(signature):
+            return self.send_complex_response(403, 'Invalid signature')
+        event = self.headers.get('X-Github-Event')
+        if event == 'ping':
+            return self.send_complex_response(200, 'Success')
+        elif event == 'push':
+                branch = payloadj['ref'].split('/')[2]
+                command = config[repository][event][branch]['command']
+                try:
+                    body = check_output(command).decode('utf-8')
+                    print(f'{command}: {body}')
+                    return self.send_complex_response(200, 'Success', body)
+                except Exception as e:
+                    return self.send_complex_response(500, e)
         else:
-            self.send_complex_response(403, 'No "X-Hub-Signature" signature')
+            return self.send_complex_response(500, 'Unsupported event')
 
     def send_complex_response(self, code, message, body=None):
         self.send_response(code, message)
